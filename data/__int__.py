5559f0666b215df8cc4ff9f4329b50a8c62e6f45
Mazen Alotaibi <mazen.ota@gmail.com>
2018-10-31 21:41:01 -0700

    tried to import data for data analysis

---
+++ data/__int__.py
@@ -0,0 +1,62 @@
+# import libraries
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+import tensorflow as tf
+import numpy as np
+import sys
+import os
+
+# import files
+import helper as hp
+
+# make sure everything was written properly by reading it back out
+def read_and_decode_single_example(filenames):
+    filename_queue = tf.train.string_input_producer(filenames, num_epochs=1)
+    
+    reader = tf.TFRecordReader()
+    
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(
+        serialized_example,
+        features={
+            'label_normal': tf.FixedLenFeature([], tf.int64),
+            'image': tf.FixedLenFeature([], tf.string)
+        })
+    
+    # now return the converted data
+    label = features['label_normal']
+    image = tf.decode_raw(features['image'], tf.uint8)
+    image = tf.reshape(image, [299, 299, 1])
+    
+    return label, image
+
+# reading data
+# TODO: can't read the data
+# ERROR:
+#   tf.data
+label, image = read_and_decode_single_example(["/raw/training/training10_0.tfrecords", "../input/training10_1/training10_1.tfrecords"])
+images_batch, labels_batch = tf.train.batch([image, label], batch_size=16, capacity=2000)
+global_step = tf.Variable(0, trainable=False)
+
+# view data
+# TODO: can't view the data
+# ERROR:
+#   CPU settings, use GPU or updated version for CPU
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
+    
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+    for j in range(3):
+        la_b, im_b = sess.run([labels_batch, images_batch])
+        
+        for i in range(8):
+            plt.imshow(im_b[i].reshape([299,299]))
+            plt.title("Label: " + str(la_b[i]))
+            plt.show()
+            
+    coord.request_stop()
+    
+    # Wait for threads to stop
+    coord.join(threads)
